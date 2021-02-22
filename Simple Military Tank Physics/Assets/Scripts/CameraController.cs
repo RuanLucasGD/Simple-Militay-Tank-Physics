b61d41e4640281e05d2872c78690302193211bfd
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2021-02-22 16:52:39 -0400

    Added vehicle camera controller

---
+++ Simple Military Tank Physics/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float rotSpeed;
+    public float camZoomSpeed;
+    public float camHeight;
+
+    private Transform cam;
+
+    public float maxCamDistance;
+    public float minCamDistance;
+
+    void Start()
+    {
+        cam = GetComponentInChildren<Camera>().transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void FixedUpdate()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(((Vector3.up * mouseX) + (Vector3.right * mouseY)) * rotSpeed);
+
+        Vector3 cameraLookAt = target.position + (Vector3.up * camHeight);
+
+        cam.LookAt(cameraLookAt);
+        transform.position = target.position;
+    }
+}
