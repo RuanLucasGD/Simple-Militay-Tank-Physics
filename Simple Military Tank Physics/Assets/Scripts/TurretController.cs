6422d1e2349f4e5b234b42805706e2182f5129de
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2021-02-22 23:05:08 -0400

    Added gun rotation limit

--- Simple Military Tank Physics/Assets/Scripts/TurretController.cs
+++ Simple Military Tank Physics/Assets/Scripts/TurretController.cs
@@ -8,12 +8,18 @@ public class TurretController : MonoBehaviour
     public Transform turret;
     public Transform gun;
 
+    public float rotSpeedHorizontal;
+    public float rotSpeedVertical;
+
+    public float maxGunAngle;
+    public float minGunAngle;
+
     void Start()
     {
 
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Vector3 targetPos = cameraController.GetTargetPosition();
 
@@ -25,8 +31,30 @@ public class TurretController : MonoBehaviour
         Quaternion turretRot = Quaternion.Euler(vehicle.eulerAngles - turretRotDelta.eulerAngles);
         Quaternion gunRot = Quaternion.Euler(turret.eulerAngles - turretRotDelta.eulerAngles);
 
+        turretRot = Quaternion.Slerp(turret.rotation, turretRot, rotSpeedHorizontal);
+        gunRot = Quaternion.Slerp(gun.rotation, gunRot, rotSpeedVertical);
+
         turret.rotation = turretRot;
         gun.rotation = gunRot;
+
+        float max = 360 - maxGunAngle;
+        float min = minGunAngle;
+        float currentAngle = gun.localEulerAngles.x;
+
+        if (currentAngle > 180)
+        {
+            if (currentAngle < max)
+            {
+                gun.transform.localEulerAngles = new Vector3(max, 0, 0);
+            }
+        }
+        else
+        {
+            if (currentAngle > min)
+            {
+                gun.transform.localEulerAngles = new Vector3(min, 0, 0);
+            }
+        }
 
         turret.localRotation = Quaternion.Euler(0, turret.localEulerAngles.y, 0);
         gun.localRotation = Quaternion.Euler(gun.localEulerAngles.x, 0, 0);
