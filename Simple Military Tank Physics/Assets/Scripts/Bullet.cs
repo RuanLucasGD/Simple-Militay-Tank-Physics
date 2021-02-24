fdc9a4689f397ce97029643596d347715a3eafd0
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2021-02-24 09:51:17 -0400

    Added gun shoot and bullet

---
+++ Simple Military Tank Physics/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Start()
+    {
+        Destroy(gameObject, 10);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Simple Military Tank Physics/Assets/Scripts/Shoot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+}
